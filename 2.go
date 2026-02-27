d40849d5c49742ddeb80f7467778cc47716c5358
Aditya Kumar Sethi <progroup@atomicmail.io>
2026-02-27 11:29:46 +0530

    Add initial Hello World program in 2.go

---
+++ 2.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+hgvtvtfpackage main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+
