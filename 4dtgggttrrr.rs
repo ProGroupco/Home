4420a087fba1c4d21f6686b2ee821806c06a410a
Aditya Kumar Sethi <progroup@atomicmail.io>
2026-03-03 13:19:50 +0530

    Add Rust program to print the alphabet
    This file contains a Rust program that prints the alphabet multiple times.

---
+++ 4dtgggttrrr.rs
@@ -0,0 +1,34 @@
+fn main() {
+    // Rust Manual Code Segment
+    println!("Line 1: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 2: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 3: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 4: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 5: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 6: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 7: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 8: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 9: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 10: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 11: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 12: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 13: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 14: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 15: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 16: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 17: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 18: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 19: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 20: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 21: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 22: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 23: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 24: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 25: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 26: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 27: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 28: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 29: abcdefghijklmnopqrstuvwxyz");
+    println!("Line 30: abcdefghijklmnopqrstuvwxyz");
+    // ... ଏହିପରି ଆପଣ Ctrl+V କରି ୨୦୦୦ ଧାଡ଼ି କରିପାରିବେ ...
+}
